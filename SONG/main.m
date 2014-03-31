b34f23eb7ba8f5b647e63957200589c9601d2f3f
asm11 <asm11@asm11ui-iMac.local>
2014-03-31 16:06:16 +0900

    Initial Commit

---
+++ SONG/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SONG
+//
+//  Created by asm11 on 2014. 3. 31..
+//  Copyright (c) 2014년 asm11. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
